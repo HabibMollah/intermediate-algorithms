ef060361a08ea184ec3b496a6471e34fee472b67
HabibMollah <hrmollah2002@gmail.com>
2023-03-21 19:51:46 +0600

    project 01 added

---
+++ 01-sum-all-nums-in-range.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+	arr.sort((a, b) => a - b);
+	let sum = 0;
+	console.log(arr);
+	for (let i = arr[0]; i <= arr[1]; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+console.log(sumAll([40, 1]));
