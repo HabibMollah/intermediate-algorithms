5a03be0a442c7c6be96b14e69d45552cc747af73
HabibMollah <hrmollah2002@gmail.com>
2023-04-08 14:21:31 +0600

    caeser's cipher

--- 05-caeser's-cipher.js
+++ 05-caeser's-cipher.js
@@ -1,5 +1,64 @@
 function rot13(str) {
-	return str;
+	let alphabet = [
+		'A',
+		'B',
+		'C',
+		'D',
+		'E',
+		'F',
+		'G',
+		'H',
+		'I',
+		'J',
+		'K',
+		'L',
+		'M',
+		'N',
+		'O',
+		'P',
+		'Q',
+		'R',
+		'S',
+		'T',
+		'U',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z',
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+	];
+	console.log();
+	return [...str]
+		.map((x) => {
+			if (!alphabet.includes(x)) return x;
+			return alphabet[alphabet.indexOf(x) + 13];
+		})
+		.join('')
+		.toUpperCase();
 }
-
-rot13('SERR PBQR PNZC');
